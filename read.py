be7ffef08ae7a3c9db18e9994dd138bb9051f85d
ToonSantermans <toon.santermans@student.kuleuven.be>
2015-10-15 12:24:41 +0200

    New exercise!

---
+++ read.py
@@ -0,0 +1,13 @@
+from __future__ import print_function, division
+import numpy as np
+import sys
+
+
+N = int(sys.argv[1])
+
+print("N is ", N)
+
+for number in np.random.normal(size=N):
+	print(number)
+
+
